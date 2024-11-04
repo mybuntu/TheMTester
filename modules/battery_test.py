b1bdaece416f025219d2c587a4c69f872012bc4b
mybuntu <101842543+mybuntu@users.noreply.github.com>
2024-11-04 19:37:41 +0100

    Update battery_test.py

--- modules/battery_test.py
+++ modules/battery_test.py
@@ -7,16 +7,16 @@ import os
 # Fonction pour obtenir les informations sur la batterie et les écrire dans un fichier texte
 def check_battery_status(output_file):
     try:
-        # Exécuter la commande ioreg pour obtenir les capacités
-        ioreg_output = subprocess.check_output("ioreg -l -b | grep -i -E 'capacity|CycleCount'", shell=True).decode('utf-8')
+        # Exécuter la commande pmset pour obtenir les capacités
+        pmset_output = subprocess.check_output("pmset -g batt", shell=True).decode('utf-8')
 
         # Extraire les capacités
-        max_capacity = int(re.search(r'"MaxCapacity" = (\d+)', ioreg_output).group(1))
-        current_capacity = int(re.search(r'"CurrentCapacity" = (\d+)', ioreg_output).group(1))
-        design_capacity = int(re.search(r'"DesignCapacity" = (\d+)', ioreg_output).group(1))
+        max_capacity = int(re.search(r'"MaxCapacity" = (\d+)', pmset_output).group(1))
+        current_capacity = int(re.search(r'"CurrentCapacity" = (\d+)', pmset_output).group(1))
+        design_capacity = int(re.search(r'"DesignCapacity" = (\d+)', pmset_output).group(1))
 
         # Extraire le nombre de cycles de charge
-        cycle_count_match = re.search(r'"CycleCount" = (\d+)', ioreg_output)
+        cycle_count_match = re.search(r'"CycleCount" = (\d+)', pmset_output)
         cycle_count = int(cycle_count_match.group(1)) if cycle_count_match else "Non disponible"
 
         # Calculer la santé de la batterie en pourcentage
